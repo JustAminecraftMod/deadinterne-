26c8813312fdb016013549d7a1038f3ea85a241e
Andrii Ivanov <ivanovandrii46@gmail.com>
2024-08-14 22:42:46 +0100

    Added minimal jonkler code :( sad

---
+++ src/main/java/jonkler/item/ModItems.java
@@ -0,0 +1,10 @@
+package jonkler;
+
+import net.minecraft.world.item.Item;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+
+public class mod {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, deadInternet.MOD_ID)
+}
